fd82b8de465fca80f6b220434d3128fc1af6aa94
Felix Trautwein <mail@felixtrautwein.de>
2025-11-26 17:30:26 +0100

    chore: Clean up devenv

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -9,6 +9,3 @@ source .venv/bin/activate
 uv sync
 
 pre-commit install >/dev/null 2>&1
-git lfs install >/dev/null 2>&1
-git lfs track "*.png" >/dev/null 2>&1
-git lfs track "*.jpg" >/dev/null 2>&1
